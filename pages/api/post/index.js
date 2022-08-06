1570b99d11f7c42bcdfe81734cc74d6792655510
kunal patel <pkunal257@gmail.com>
2022-08-06 11:41:41 +0530

    main section added

---
+++ pages/api/post/index.js
@@ -0,0 +1,12 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import {allPostsQuery} from "../../../utils/queries"
+import {client} from "../../../utils/client"
+export default async function handler  (req,res) {
+  if(req.method == 'GET'){
+    const query = allPostsQuery();
+    const data = await client.fetch(query);
+    res.status(200).json(data);
+  }
+  
+}
